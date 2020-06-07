6b210c03c4297229b123178db3f759f0e8b522eb
changhao2882 <changhao2882@163.com>
2020-06-07 20:14:08 +0800

    提交测试

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.atguigu.gmall.user.controller;
 
+import com.atguigu.gmall.user.bean.UmsMember;
 import com.atguigu.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -16,6 +19,14 @@ public class UserController {
     @ResponseBody
     public String index(){
         return "hello user";
+    }
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
     }
 
 }
